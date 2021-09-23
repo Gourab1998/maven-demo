0b7202dbe3526461df1e2ba884548b4bc166634d
I524895 <gourab.mukherjee@sap.com>
2021-09-23 14:04:26 +0530

    module dependency added

---
+++ HelloWorldClient/src/main/java/HelloWorldClient.java
@@ -0,0 +1,8 @@
+import helloworld.HelloWorldServer;
+public class HelloWorldClient {
+    public static void main (String args[]) {
+        HelloWorldServer obj = new HelloWorldServer();
+        obj.print("Bits");
+        //System.out.println("Hello");
+    }
+}
---
+++ HelloWorldServer/src/main/java/HelloWorldServer.java
@@ -0,0 +1,6 @@
+package helloworld;
+public class HelloWorldServer {
+    public void print (String name) {
+        System.out.println("Hello " + name);
+    }
+}
